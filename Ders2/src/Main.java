efd36115d1268a20761698ac85c80b8b35871b78
nihan.toktas <nihantkts_@hotmail.com>
2019-01-27 14:14:05 +0300

    ilkel ve tip dönüşümleri

---
+++ Ders2/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int intb;
+
+        intb = 127;
+        byte bytea = (byte) intb;
+        System.out.println("maximum boyutu geçmediğim tip dönüşümü");
+       Integer xInt=12;
+       String xString;
+       xString=xInt.toString();
+       System.out.println(xString);
+
+
+
+
+    }
+
+
+}
+
+
+
