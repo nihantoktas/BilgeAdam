269d6fca3935a2e068092d1014fe6db213dec18e
nihan.toktas <nihantkts_@hotmail.com>
2019-02-02 12:39:21 +0300

    aritmatik ve ilişkisel karşılaştırma işlemleri

---
+++ Ders3/src/aritmatikoperatorler/IliskiselKarsılastirmaApp.java
@@ -0,0 +1,23 @@
+package aritmatikoperatorler;
+
+import java.sql.SQLOutput;
+
+public class IliskiselKarsılastirmaApp {
+    public static void main(String[] args) {
+        int a=5,b=10;
+        //<,<=,>,>=,==.!=
+        boolean sonuc;
+        sonuc=a<b
+        System.out.println("A'nın B'den küçük olma durumu: "+sonuc);
+        System.out.println(a<b);
+        sonuc=a>b;
+        System.out.println("A'nın c'den büyük olma durumu: "+sonuc);
+        sonuc=a<=b;
+        System.out.println("A'nın B'den küçük eşit olma durumu: "+sonuc);
+        sonuc=a>=b;
+        System.out.println("A'nın B'den büyük eşit olma durumu: "+sonuc);
+        sonuc=a==b;
+        System.out.println("A'nın B'den eşit olma durumu: "+sonuc);
+        sonuc=a!=b;
+    }
+}
